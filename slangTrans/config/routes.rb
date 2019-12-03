83d65821f04169204524e3c653a3f98fdd11916a
fanzhong <fanzhongzeng78@gmail.com>
2019-12-03 18:03:04 -0500

    login session complete

--- slangTrans/config/routes.rb
+++ slangTrans/config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   resources :slangs
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/", to: "users#index", as: "main_menu"
+  get "/login", to:"users#login", as: "login"
+  post '/login' => 'users#create'
+  post '/logout' => 'users#destroy'
 end
