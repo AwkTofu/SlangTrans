482bb9d8859af4a9fc4f7dfc43e55a0146f5f5c5
GAierken <g.aierken@gmail.com>
2019-12-04 11:32:27 -0500

    added fields_for in slangs_edit

--- slangTrans/app/controllers/slangs_controller.rb
+++ slangTrans/app/controllers/slangs_controller.rb
@@ -15,21 +15,15 @@ class SlangsController < ApplicationController
 
     def edit
         find_slang
-        mandarin_one
-        english_one
-        german_one
+        
         
     end 
 
     def update
         find_slang
-        mandarin_one
-        english_one
-        german_one
+       
         @slang.update(slang_params)
-        @mandarin.update(params)
-        @english.update(params)
-        @german.update(params)
+        
      
         redirect_to slang_path(@slang)
     end 
@@ -46,26 +40,12 @@ class SlangsController < ApplicationController
 
     private
     def slang_params
-        params.require(:slang).permit(:phrase, :origin)
+        params.require(:slang).permit(:phrase, :user_id, :origin, definition_ids:[], definitions_attributes: [:meaning])
     end 
 
     def find_slang
         @slang=Slang.find(params[:id])
     end 
 
-    def mandarin_one
-        find_slang
-        @mandarin = @slang.definitions.find_by(language: 'mandarin')
-    end 
-
-    def english_one
-        find_slang
-        @english = @slang.definitions.find_by(language: 'english')
-    end 
-    
-    def german_one
-        find_slang
-        @german = @slang.definitions.find_by(language: 'german')
-    end 
-
+   
 end
--- slangTrans/app/models/slang.rb
+++ slangTrans/app/models/slang.rb
@@ -3,4 +3,5 @@ class Slang < ApplicationRecord
   has_many :definitions
   has_many :slang_books
   has_many :books, through: :slang_books
+  accepts_nested_attributes_for :definitions
 end
