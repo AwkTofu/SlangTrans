441088af10b32018fd9d1f9d2fa9df8a68d65205
fanzhong <fanzhongzeng78@gmail.com>
2019-12-05 10:26:46 -0500

    added login on every page

--- slangTrans/app/controllers/users_controller.rb
+++ slangTrans/app/controllers/users_controller.rb
@@ -11,6 +11,21 @@ class UsersController < ApplicationController
 
   	#data gather from creating the form
   	def create
+  		#If password field isn't filledout, give error, and redirect
+  		if params[:user][:password] == ""
+  			flash[:notice] = "Password field cannot be empty"
+  			redirect_to new_user_path
+  		end
+
+  		#White listing the params and making the user
+  		wl = params[:user].permit(:name, :username, :password)
+  		user = User.new(wl) 
+
+  		if user.valid?
+
+  		else
+  			flash[:notice] = "Some Field was not filled out"
+  		end
   		byebug#
   	end
 end
